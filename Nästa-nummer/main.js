222be80f3b15e3c08fad5c75aee36f2936d1cbde
sara <sara.ekmanMedieinstitutet@gmail.com>
2021-12-18 20:28:39 +0100

    done next num

--- Nästa-nummer/main.js
+++ Nästa-nummer/main.js
@@ -2,12 +2,5 @@ const input = document.querySelector("input");
 const button = document.querySelector("button")
 const header = document.querySelector("h1");
 
-function secondNum(num) {
-    let nextNum = Number(num) + 1
-    header.innerText = nextNum
-}
-
-button.addEventListener("click", () => {
-    const inputValue = input.value
-    secondNum(inputValue)
-})
+button.addEventListener("click", () => 
+    header.innerText = +input.value + 1)
